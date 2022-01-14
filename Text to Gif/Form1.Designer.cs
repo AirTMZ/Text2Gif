8c3f2f2ca1806da3bc5eb260d80a0b4fba2bcca6
AirTMZ <50797752+AirTMZ@users.noreply.github.com>
2022-01-14 11:48:10 +0000

    Add files via upload

---
+++ Text to Gif/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Text_to_Gif
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Generate = new System.Windows.Forms.Button();
+            this.input = new System.Windows.Forms.TextBox();
+            this.Giphy = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Giphy)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            this.Generate.BackColor = System.Drawing.Color.White;
+            this.Generate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Generate.Location = new System.Drawing.Point(93, 228);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(75, 23);
+            this.Generate.TabIndex = 0;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = false;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // input
+            // 
+            this.input.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.input.Location = new System.Drawing.Point(93, 162);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(159, 26);
+            this.input.TabIndex = 1;
+            this.input.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Input_KeyUp);
+            // 
+            // Giphy
+            // 
+            this.Giphy.Location = new System.Drawing.Point(571, 138);
+            this.Giphy.Name = "Giphy";
+            this.Giphy.Size = new System.Drawing.Size(100, 50);
+            this.Giphy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.Giphy.TabIndex = 2;
+            this.Giphy.TabStop = false;
+            this.Giphy.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 588);
+            this.Controls.Add(this.Giphy);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.Generate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Giphy)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.PictureBox Giphy;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Text to Gif/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Drawing;
+using System.Net;
+using System.Windows.Forms;
+namespace Text_to_Gif
+{
+    public partial class Form1 : Form
+    {
+        public string[] words;
+        public bool check = true;
+        public string sizeX;
+        public string sizeY;
+        public int x;
+        public int y;
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = Color.FromArgb(137, 207, 240);
+        }
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Start();
+                Giphy.Hide();
+                string link = $"https://api.giphy.com/v1/gifs/search?api_key=EHW8QzWFddimzox00RxDMCx3wzZdjKj4&q={input.Text}&limit=1&offset=0&rating=g&lang=en";
+                string json = new WebClient().DownloadString(link);
+                JObject o = JObject.Parse(json);
+
+                string gif = (string)o["data"][0]["images"]["original"]["url"];
+                sizeX = (string)o["data"][0]["images"]["original"]["height"];
+                sizeY = (string)o["data"][0]["images"]["original"]["width"];
+
+                Giphy.ImageLocation = gif;
+                x = (this.Width / 2) - (int.Parse(sizeX) / 2);
+                y = (this.Height / 2) - (int.Parse(sizeY) / 2);
+                Giphy.Location = new Point(x, y);
+
+                Giphy.Show();
+                input.Text = "";
+            }
+            catch
+            {
+            }
+        }
+        private void Input_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+            {
+                Generate_Click(this,e);
+            }
+        }
+    }
+}
